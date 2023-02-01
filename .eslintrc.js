456fc268d047118d72acf6fbfb022c1349cd0a1c
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-02-01 08:02:26 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        "plugin:eslint-comments/recommended",
+        "plugin:import/recommended",
+        "prettier"
+    ],
+    rules: {
+        "eslint-comments/disable-enable-pair": ["error", {"allowWholeFile": true}],
+        "eslint-comments/no-aggregating-enable": "error",
+        "eslint-comments/no-duplicate-disable": "error",
+        "eslint-comments/no-unlimited-disable": "error",
+    }
+};
