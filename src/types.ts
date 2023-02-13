a3fbf1045efcf019e0909069e83945aa405187b4
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-02-13 10:05:00 +0100

    fixed length array

---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type ArrayLength<T extends any[]> = T['length'];
+export type ComposeArray<A extends any[], B extends any[]> = [...A, ...B];
+export type Push<T, A extends T[], V extends T> = [...A, V];
+
+export type FixedLengthArray<T, L extends number, P extends T[] = []> = L extends P["length"] ? P : FixedLengthArray<T, L, Push<T, P, T>>;
