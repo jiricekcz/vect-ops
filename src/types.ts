03b8c3ce224ab8b2a01ed22426752d5109ba80c2
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-02-13 10:12:11 +0100

    swap of extends

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,4 @@ export type ArrayLength<T extends any[]> = T['length'];
 export type ComposeArray<A extends any[], B extends any[]> = [...A, ...B];
 export type Push<T, A extends T[], V extends T> = [...A, V];
 
-export type FixedLengthArray<T, L extends number, P extends T[] = []> = L extends number ? T[] :L extends P["length"] ? P : FixedLengthArray<T, L, Push<T, P, T>>;
+export type FixedLengthArray<T, L extends number, P extends T[] = []> = number extends L ? T[] : L extends P["length"] ? P : FixedLengthArray<T, L, Push<T, P, T>>;
