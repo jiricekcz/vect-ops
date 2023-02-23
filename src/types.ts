1528ade2a03bb02e545e5aa85dedb0c63da35ee7
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-02-23 08:11:14 +0100

    at least length array

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,5 @@ export type ComposeArray<A extends any[], B extends any[]> = [...A, ...B];
 export type Push<T, A extends T[], V extends T> = [...A, V];
 
 export type FixedLengthArray<T, L extends number, P extends T[] = []> = number extends L ? T[] : L extends P["length"] ? P : FixedLengthArray<T, L, Push<T, P, T>>;
+export type ArrayWithAtLeastLength<T, L extends number> = [...FixedLengthArray<T, L>, ...T[]];
+
